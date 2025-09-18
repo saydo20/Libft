3fcadf255f9109716c7143087d17c45c4f829750
saad <jdiasaad86@gmail.com>
2025-09-18 17:44:24 +0100

    done the calloc function

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saad <saad@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 17:36:50 by saad              #+#    #+#             */
+/*   Updated: 2025/09/18 17:43:09 by saad             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	size_t total;
+	void *ptr;
+
+	total = nmemb * size;
+	if(nmemb != 0 || total / nmemb != size)
+		return NULL;
+	ptr = malloc(total);
+	if(!ptr)
+		return NULL;
+	ft_bzero(ptr, total);
+	return ptr;
+}
