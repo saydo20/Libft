b415f84e04a98249424125cc5c0b1e93813d0d61
saad <jdiasaad86@gmail.com>
2025-09-19 13:09:30 +0100

    finish the substr function

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saad <saad@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 12:39:45 by saad              #+#    #+#             */
+/*   Updated: 2025/09/19 13:08:07 by saad             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t s_len;
+	char *ptr;
+	size_t i;
+
+	if(!s)
+		return NULL;
+	if(s[0] == '\0')
+		return ft_strdup("");
+	s_len = ft_strlen(s);
+	if(len > s_len - start)
+		len = s_len - start;
+	ptr = malloc(len + 1);
+	if(!ptr)
+		return NULL;
+	i = 0;
+	while(i < len)
+	{
+		ptr[i] = s[i + start];
+		i++;
+	}
+	ptr[i] = '\0';
+	return ptr;
+}
